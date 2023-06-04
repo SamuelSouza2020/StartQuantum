f2ea114f5de377953aa03f20dc936e437946cdbd
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2023-06-04 13:19:07 -0300

    quantum inicio

---
+++ photon-quantum-sdk_v2.1.6-stable/quantum_code/quantum.code/Platformer/MovementSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantum.Platformer
+{
+    public unsafe class MovementSystem : SystemMainThreadFilter<MovementSystem.Filter>
+    {
+        public struct Filter
+        {
+            public EntityRef Entity;
+            public CharacterController3D* CharacterController;
+        }
+        public override void Update(Frame f, ref Filter filter)
+        {
+            // gets the input for player 0
+            var input = *f.GetPlayerInput(0);
+
+            if (input.Jump.WasPressed)
+            {
+                filter.CharacterController->Jump(f);
+            }
+
+            filter.CharacterController->Move(f, filter.Entity, input.Direction.XOY);
+        }
+    }
+}
